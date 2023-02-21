fdbc37bdc50d7ed6fb9f0123148342482e9b7590
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-02-21 21:12:27 +0800

    Answered Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-function sum(n1, n2){return n1 + n2;} const x = sum(1,1); console.log(x);
+function sum(n1, n2) {
+  return n1 + n2;
+}
+const x = sum(1, 1);
+console.log(x);
+
+const a = 5;
+const b = 20;
+const c = 30;
+
+const first2Sum = sum(a + b);
+console.log(first2Sum);
+
+function product(n1, n2, n3) {
+  return n1 * n2 * n3;
+}
+const productTotal = product(a, b, c);
+console.log(productTotal);
